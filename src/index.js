0bc97f6ded99e7b6b9171a32ecb8503889c11a55
Ben Stahl <bens@vimeo.com>
2017-02-06 12:45:04 -0500

    Options should be an object

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-
 /**
  * Higher order function that makes any promise-returning-function
  * retryable with a jittr'd exponential backoff.
  *
- * @param  {Function} fn            Function to be made retryable.
- * @param  {Number}   maxRetries    Total number of retries.
- * @param  {Number}   retryCount    Current retry count.
+ * @param  {Function} fn                    Function to be made retryable.
+ * @param  {Object}   options               Configuration.
+ * @param  {Number}   options.maxRetries    Total number of retries.
+ * @param  {Number}   options.retryCount    Current retry count.
  * @return {Function}
  */
-export default function retryFunctionOnReject(fn, maxRetries = 2, retryCount = 0) {
+export default function retryFunctionOnReject(fn, options) {
+    options = Object.assign({ maxRetries: 2, retryCount: 0 }, options);
     return (...args) => {
         return new Promise((resolve, reject) => {
             fn(...args)
             .then(resolve)
             .catch((error) => {
-                const count = retryCount + 1;
-                if (count > maxRetries) {
+                options.retryCount++;
+                if (options.retryCount > options.maxRetries) {
                     return reject(error);
                 }
-                const delay = (Math.pow(2, count) * 1000) + (Math.round(Math.random() * 1000));
-                const nextFn = retryFunctionOnReject(fn, maxRetries, count);
+                const delay = (Math.pow(2, options.retryCount) * 1000) + (Math.round(Math.random() * 1000));
+                const nextFn = retryFunctionOnReject(fn, options);
                 return setTimeout(() => {
                     nextFn(...args).then(resolve).catch(reject);
                 }, delay);
